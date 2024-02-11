cedf431120cb34003f791bae94f9c056154aca82
Upasana <112039921+Igdtuwresource@users.noreply.github.com>
2024-02-12 01:22:16 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = "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"; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
